212aa4455a7be7ee87481c1604f17bf3b1f9465d
Amit Kumar <alsoamit@gmail.com>
2023-04-03 14:03:20 +0530

    feat: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from 'src/layout';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+    `}
+      </Script>
+    </>
   );
 }
