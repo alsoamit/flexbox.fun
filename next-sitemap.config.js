c45ec6eedb2d727f17081dbeba9ce521369d8a99
Amit Kumar <alsoamit@gmail.com>
2023-04-03 13:39:50 +0530

    feat: sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://flexbox.fun',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
