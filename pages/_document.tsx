69ffa84e7fc0acaddb2c15a52dcabaad165740c0
Amit Kumar <alsoamit@gmail.com>
2024-01-10 23:20:44 +0530

    feat: clarity analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html className="dark">
-        <Head />
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "kjrtd6p769");`,
+            }}
+          />
+        </Head>
         <body className="min-h-screen w-full max-w-full dark:bg-gray-900">
           <Main />
           <NextScript />
